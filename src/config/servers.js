edaed27c574b9eafb623f8ab585cfafd73bfdb2e
Dylanrock333 <dylanf36200@gmail.com>
2026-09-13 19:26:34 -0500

    Fill in real Discord guild/channel IDs and the fantasy-bot league ID in servers.js, replacing the placeholder values used before the actual server config was known.

--- src/config/servers.js
+++ src/config/servers.js
@@ -2,8 +2,8 @@
 // fantasy channel it talks in and the league it's tied to on fantasy-bot.
 // TODO: move this to a database once we need more than a couple servers.
 const SERVERS = {
-  "GUILD_ID_A": { channelId: "CHANNEL_ID_A", leagueId: "LEAGUE_ID_A" },
-  "GUILD_ID_B": { channelId: "CHANNEL_ID_B", leagueId: "LEAGUE_ID_B" },
+  "1544547612659679294": { channelId: "1544548090025877604", leagueId: 1992397255 },
+  "1547852208677199943": { channelId: "1548100929906151537", leagueId: 1992397255 },
 };
 
 function getServerConfig(guildId) {
